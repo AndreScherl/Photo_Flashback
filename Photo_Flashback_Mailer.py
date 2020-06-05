89a72d89af7ec329cd74ea601fd56e1f36c5b4e0
Andre Scherl <post@schoerlock.de>
2020-06-05 20:39:15 +0200

    first running version

---
+++ Photo_Flashback_Mailer.py
@@ -0,0 +1,47 @@
+import config
+from datetime import datetime
+import os.path
+
+# Module to get the file list
+import glob
+
+# Modules to send emails
+import smtplib
+#from email.message import EmailMessage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+
+# Module to find the types of our images
+#import imghdr
+
+# Get files of directory with day and month matching to the current date. finish, if there are noch matching files.
+today = datetime.now().strftime('%m-%d')
+print(config.directory)
+files_of_today = glob.glob(config.directory + "*-" + today + "*.*")
+print(files_of_today)
+
+# Set up an email with embedded pictures/movies
+#msg = EmailMessage()
+msg = MIMEMultipart()
+msg['Subject'] = 'Zeitreise'
+msg['From'] = config.mail_from
+msg['To'] = ', '.join(config.mail_to)
+
+# Open the files in binary mode.  Use imghdr to figure out the MIME subtype for each specific image.
+for file in files_of_today:
+    (filedir, filename) = os.path.split(file)
+    msgText = MIMEText('<img src="cid:%s"><br>' % (filename), 'html') 
+    msg.attach(msgText)
+
+    with open(file, 'rb') as fp:
+        img = MIMEImage(fp.read())
+    
+    img.add_header('Content-ID', '<{}>'.format(filename))
+    msg.attach(img)
+    #msg.add_attachment(img_data, maintype='image', subtype=imghdr.what(None, img_data))
+
+# Send mail.
+with smtplib.SMTP(config.smtp_server, config.smtp_port) as s:
+    s.login(config.smtp_user, config.smtp_pass)
+    s.send_message(msg)
---
+++ sample.config.py
@@ -0,0 +1,14 @@
+# Rename this file to config.py to get it loaded.
+
+# Directory containing the files, named yyyy-mm-dd.suffix or yyyy-mm-dd_<number>.suffix
+directory = "/some/path/to/your/files/"
+
+# SMTP Server
+smtp_server = "smtp.somedomain.com"
+smtp_port = "587"
+smtp_user = "email@somedomain.com"
+smtp_pass = "password_for_this_mail_account"
+
+# Email Adresses
+mail_from = "me@somedomain.com"
+mail_to = ["you1@somedomain.com", "you2@somedomain.com"]
